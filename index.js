ff76b2ac98bb34b7f1ff9436879cb1d399633c58
Tim Flood <tflood9@gmail.com>
2021-03-09 09:28:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  speak() {
+    switch (this.sex) {
+      case "male":
+        return `It's me! ${this.name}, the parrot!`;
+        break;
+      case "female":
+        return `${this.name} says squawk!`;
+    }
+  }
+}
